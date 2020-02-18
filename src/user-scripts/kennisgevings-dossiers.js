93a4c09070f01d008cbe0d59ff7b1993e9f987cf
CounterFlame <blabla>
2020-02-19 00:00:15 +0100

    Refactor structure + add actual user script

---
+++ src/user-scripts/kennisgevings-dossiers.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         EVOA - Kennisgevingsdossiers
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Kennisgevingsdossier specific actions
+// @author       Yentl Storms
+// @match        https://localhost:9000/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// @require      https://gist.githubusercontent.com/raw/2625891/waitForKeyElements.js
+// @require      http://www.openjs.com/scripts/events/keyboard_shortcuts/shortcut.js
+// @require      https://raw.githubusercontent.com/yentlprojects/ovam-userscript/master/util.js
+// @require      https://raw.githubusercontent.com/yentlprojects/ovam-userscript/master/sidebar.js
+// @require      https://raw.githubusercontent.com/yentlprojects/ovam-userscript/master/kennisgevingen/dossier.js
+// ==/UserScript==
+
+/* globals waitForKeyElements */
+/* globals initSidebar, createSidebarButton */
+/* globals dossierActions */
+
+(function() {
+    'use strict';
+
+    waitForKeyElements('nav.vl-side-navigation:contains("Contact"):contains("Dossierkost"):contains("Bankgarantie"):contains("Vakken")', () => {
+        const sidebar = initSidebar();
+        sidebar.empty();
+        dossierActions.forEach(({name, execute}) => sidebar.append(createSidebarButton(name, execute)));
+    });
+})();
