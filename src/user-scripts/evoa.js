e9be88ec7ef30fb7dcf6a5ceaa5bd3094b4d79b3
CounterFlame <blabla>
2020-04-07 11:16:55 +0200

    Update Vak-15 input logic

--- src/user-scripts/evoa.js
+++ src/user-scripts/evoa.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EVOA
 // @namespace    http://tampermonkey.net/
-// @version      0.10
+// @version      0.12
 // @description  Kennisgevingsdossier specific actions
 // @author       Yentl Storms
 // @match        https://*.evoa.ovam.be/*
