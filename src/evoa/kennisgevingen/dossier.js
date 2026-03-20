3daaeab339216f4233c5447f53f04b08460a28b2
StijnVh <stijn.vanhoorelbeke@gmail.com>
2026-03-20 10:42:00 +0100

    Update dossier.js

--- src/evoa/kennisgevingen/dossier.js
+++ src/evoa/kennisgevingen/dossier.js
@@ -278,7 +278,7 @@ async function fillInVak6(save = true) {
 async function fillInVak7(save = true) {
     const vak = await waitForElementOnceById(vakken[7].id);
 
-    selectOptionByValue(vak.find('select[name="verpakkingsWijze"]'), '1');
+    selectOptionByValue(vak.find('select[name="selectedVerpakkingsWijze"]'), '1');
 
     if (save) {
         vak.find('button[type="submit"]').click();
